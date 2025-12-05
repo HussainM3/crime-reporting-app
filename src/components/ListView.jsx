e3e5a295e110a0f438a759972066bc9f3d1f86e3
Sarim Zia <54862158+sarzar@users.noreply.github.com>
2025-12-05 17:27:12 -0500

    Moved filter dropdown

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -1,6 +1,9 @@
+import FilterDropdown from "./FilterDropDown";
+
 const ListView = () => {
   return (
     <div>
+       <FilterDropdown />
       <div className="p-4">List View Content</div>
     </div>
   );
